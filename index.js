278c51409e55f7fc4a9589517bfb188ef462cf04
hucheunghugo <2503hugo@gmail.com>
2023-11-22 01:33:45 +0800

    Final 2

---
+++ index.js
@@ -0,0 +1,187 @@
+/*
+	index.js
+	Entry point for the app.
+	To start node index.js
+*/
+
+var express = require('express');
+var app = express();
+
+app.use(express.static('public'));
+
+app.use('/images', express.static(__dirname + '/images'));
+app.use('/css', express.static(__dirname + '/css'));
+
+
+app.get('/', function(req, res) {
+//	res.send("Hello World!");
+/* display index.html file */
+	res.sendFile(__dirname + "/index.html");
+
+
+app.get('/view_catalog', function(req, res) {
+		res.sendFile(__dirname + "/cart.html");
+	});
+	
+app.get('/view_shop', function(req, res) {
+		res.sendFile(__dirname + "/shop.html");
+	});
+
+app.get('/view_contact', function(req, res) {
+		res.sendFile(__dirname + "/contact.html");
+	});	
+	
+app.get('/view_approval', function(req, res) {
+		res.sendFile(__dirname + "/approval.html");
+	});		
+
+app.get('/add_cart', function(req, res) {
+//		res.send("Add Cart - need MySQL");
+
+		var responseText = 'Prod_id: ' + req.query.prod_id + '<br>';
+		responseText += 'Qty: ' + req.query.qty + '<br>';
+		responseText += 'Price: ' + req.query.price * req.query.qty + '<br>';
+		responseText += 'Username: ' + req.query.f_username + '<br><br>';
+//		res.send(responseText);
+
+	var now = new Date();
+	var cur_date_yyyy_mm_dd = now.getFullYear() + '-' + (now.getMonth()+1) + '-' + now.getDate();
+	console.log("cur_date_yyyy_mm_dd is: " + cur_date_yyyy_mm_dd);
+	var mysql = require('mysql');
+	var con = mysql.createConnection({
+		host: "localhost",
+		user: "user99",
+		password: "user99",
+		database: "com3105"
+	});
+	con.connect(function(err) {
+		if (err) throw err;
+		var sql = "INSERT INTO cart (cust_username, cart_order_date, prod_id, cart_qty, cart_price) VALUES (" +	"'" + req.query.f_username + "'," + "'" + cur_date_yyyy_mm_dd + "'," + req.query.prod_id + "," + req.query.qty + "," + req.query.price + ");";
+		console.log(sql);
+		con.query(sql, function (err, result) {
+			if (err) throw err;
+			console.log(result);
+			console.log(result.affectedRows);
+
+			if (result.affectedRows > 0) {
+				responseText += 'Thank you for your order! ' + req.query.f_username + '<br>';
+				responseText += 'The above item has been added to your shopping cart. <br>';
+			} else {
+				responseText += 'MySQL ERROR: Item not added!<br>';
+			}
+			responseText += '<br><br>';
+			responseText += '<input type="button" value = "Close this page" onclick="self.close();" />';
+			res.send(responseText);
+		}); //end of con.query()
+		con.end();
+	});  //end of con.connect()
+
+
+
+	});
+
+app.get('/check_out', function(req, res) {
+//		res.send("Check Out - need MySQL");
+/* for testing only
+		var responseText = 'Username: ' + req.query.f_check_out_username + '<br><br>';
+		res.send(responseText);
+*/
+/* paypal information 1 */
+
+	var responseText = '<!DOCTYPE html>';
+	responseText += '<head><meta name="viewport" content="width=device-width, initial-scale=1">';
+	responseText += '<meta http-equiv="X-UA-Compatible" content="IE=edge" /></head>';
+	responseText += '<body><script src="https://www.paypal.com/sdk/js?client-id=ATSWa9vavLRPYABa5DAFOb7d6xFXlYIfpC4eE0ML-fo4wvxD7MhswAQkklI625Mqnbudf6psDaPUC5mj">';
+	responseText += '</script>';
+	var mysql = require('mysql');
+	var con = mysql.createConnection({
+		host: "localhost",
+		user: "user99",
+		password: "user99",
+		database: "com3105"
+	});
+	con.connect(function(err) {
+		if (err) throw err;
+		var sql = "SELECT DATE_FORMAT(cart.cart_order_date, '%Y-%m-%d') AS order_date, ";
+		sql += "cart.prod_id, product.prod_desc, cart.cart_qty, cart.cart_price ";
+		sql += "FROM cart ";
+		sql += "INNER JOIN product ON cart.prod_id = product.prod_id ";
+		sql += "WHERE cart.cust_username = '" + req.query.f_check_out_username + "'";
+		sql += "ORDER BY order_date ASC, prod_id DESC;";		
+		console.log(sql);
+		con.query(sql, function (err, result) {
+			if (err) throw err;	
+			console.log(result);
+			responseText += 'Thank you for your order! ' + req.query.f_check_out_username + '<br>';
+			responseText += 'Your order details: <br><br>';
+			responseText += '<table border="2">';
+			responseText += '<tr><th>Image</th><th>Original Order Date</th><th>Product ID</th><th>Product Description</th><th>Quantity</th><th>Price</th><th>Amount</th></tr>';
+			var total_due = 0;
+			var sub_total = 0;
+			
+			for (var i = 0; i < result.length; i++) {
+				if(result[i].prod_id === 1){
+					imagePath = "Angus Beef.jpg";
+				}
+				
+				else if (result[i].prod_id === 2){
+					imagePath = "Norway Premium Smoked Salmon.jpg";
+					
+				}
+				
+				else {
+					imagePath = "Red King Crab.jpg";
+					
+				}
+				
+				sub_total = result[i].cart_qty * result[i].cart_price;
+				responseText += '<tr><td><img src="' + imagePath + '" alt="Product Image" width="150" height="120"></td><td>' + result[i].order_date + '</td><td>' + result[i].prod_id + '</td><td>' + result[i].prod_desc + '</td><td>' + result[i].cart_qty + '</td><td>' + result[i].cart_price + '</td><td>' + sub_total + '</td></tr>';
+
+				total_due += sub_total;
+			}
+			responseText += '</table>';
+			responseText += '<br>Total Due: ' + total_due.toFixed(2);
+			responseText += '<br><br>';
+
+/* paypal information 2 */
+
+			responseText += '<div id="paypal-button-container"></div>';
+			responseText += '<p id="txt1"></p>';
+			responseText += '<script>';
+			responseText += 'paypal.Buttons({';
+			responseText += 'createOrder: function(data, actions) {';
+			responseText += 'return actions.order.create({';
+			responseText += 'purchase_units: [{';
+			responseText += 'amount: {';
+			responseText += 'value: ' + total_due + '}';
+			responseText += '}]';
+			responseText += '});';
+			responseText += '},';
+			responseText += 'onApprove: function(data, actions) {';
+			responseText += 'return actions.order.capture().then(function(details) {';
+			responseText += 'alert("Transaction completed by " + details.payer.name.given_name);';
+			responseText += 'document.querySelector("#txt1").innerHTML = "Payment has completed! This web page can be closed now!";';
+			responseText += 'document.querySelector("#txt1").style.backgroundColor = "yellow";';
+			responseText += 'document.querySelector("#txt1").style.color = "red";';
+			responseText += '});';
+			responseText += '}';
+			responseText += '}).render("#paypal-button-container");';
+			responseText += '</script>';
+			responseText += '</body></html>';	
+			res.send(responseText);
+		}); //end of con.query()
+		con.end();
+	});  //end of con.connect()
+
+
+	});
+
+
+});
+
+
+app.listen(3000, function() {
+	console.log('index.js listening to http://127.0.0.1:3000/ or http://localhost:3000/');
+});
+
+console.log('End of Program.');
